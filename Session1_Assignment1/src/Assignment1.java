6aea139044c06c5f2393be9639c05d1ded245e71
bharathscaphy <bhrthkumar75@gmail.com>
2016-07-19 02:33:34 +0530

    First GitHub Commit
    First GitHub Commit

---
+++ Session1_Assignment1/src/Assignment1.java
@@ -0,0 +1,13 @@
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		double b=15.5;
+		int c;
+		c = (int)(a+b);
+		System.out.println(c);
+	}
+
+}
